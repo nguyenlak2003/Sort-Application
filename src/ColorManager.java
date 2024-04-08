2fd8ffd91cd2232fd6fb1e9923527b165d87e91c
nguyenlak2003 <nguyenlak1404@gmail.com>
2024-04-08 15:40:53 +0700

    update 8/4

--- src/ColorManager.java
+++ src/ColorManager.java
@@ -5,11 +5,14 @@ import java.awt.SystemColor;
  
 public class ColorManager {
     public static Color BACKGROUND = new Color(62, 62, 62);
+    public static Color SORT_BACKGROUND = new Color(232, 233, 235);
     public static Color CANVAS_BACKGROUND = new Color(48, 48, 48);
     public static Color BAR_GREEN = new Color(135, 170, 102);
     public static Color PROCESSING = new Color(244, 183, 7);
-    public static Color BASE = SystemColor.inactiveCaption;
+    public static Color BASE = new Color(232, 233, 235);
     public static Color SWAPPING = new Color(221, 77, 77);
     
     public static Color TEXT = new Color(232, 232, 232);
+    public static Color CREATE_TEXT = new Color(77, 77, 77);
+    public static Color BEFORE_SORT = new Color(11, 192, 53);  
 }
